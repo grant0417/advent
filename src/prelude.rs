f4635d9d7574fbb8837356d7cb40a28a4dbdfb3b
Grant Gurvis <grant@gurvis.net>
2024-12-02 02:38:51 -0800

    Add codspeed action

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,6 +4,7 @@ pub use crate::{
     util::math::{gcd, lcm},
     util::point::Point,
 };
+pub use itertools::Itertools;
 pub use rustc_hash::FxHashMap as HashMap;
 pub use rustc_hash::FxHashSet as HashSet;
 pub use std::fmt::Display;
